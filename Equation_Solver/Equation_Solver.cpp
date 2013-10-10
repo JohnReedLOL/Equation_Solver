84bb28a52de784ef4936d8179479a76b84544724
John-Michael Reed <JohnReedLOL@ufl.edu>
2013-10-10 11:24:47 -0400

    Update Equation_Solver.cpp

--- Equation_Solver/Equation_Solver.cpp
+++ Equation_Solver/Equation_Solver.cpp
@@ -5,7 +5,8 @@
  *      Author: John-Michael Reed
  */
  
- This file is part of the Equation_Solver source.
+ /*
+    This file is part of the Equation_Solver source.
 
     Equation_Solver is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,6 +20,7 @@
 
     You should have received a copy of the GNU General Public License
     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
     
 #include "Equation_Solver.h"
 using namespace std;
