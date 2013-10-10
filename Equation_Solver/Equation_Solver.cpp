9b527325b27a6b0e44a75f8c1dd85077c4854bee
John-Michael Reed <JohnReedLOL@ufl.edu>
2013-10-10 11:23:40 -0400

    Added license

--- Equation_Solver/Equation_Solver.cpp
+++ Equation_Solver/Equation_Solver.cpp
@@ -2,19 +2,26 @@
  * Equation_Solver.cpp
  *
  *  Created on: Oct 9, 2013
- *      Author: ubuntu
+ *      Author: John-Michael Reed
  */
+ 
+ This file is part of the Equation_Solver source.
+
+    Equation_Solver is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Equation_Solver is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    
 #include "Equation_Solver.h"
 using namespace std;
-
-//Initial changes
-//2
-//blah blah 
-int main(int argc, char **argv)
-{
-	cout << "Hello" << endl;
-	return 0;
-}
 
 
 
