240dd4fccc1739d06732b20abb1e5038f3df9167
John-Michael Reed <JohnReedLOL@ufl.edu>
2013-10-10 11:27:20 -0400

    Create main.cpp

---
+++ Equation_Solver/main.cpp
@@ -0,0 +1,34 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 10, 2013
+ *  Author: John-Michael Reed
+ */
+ 
+ /*
+    This file is part of the Equation_Solver source.
+
+    Equation_Solver is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Equation_Solver is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+  */
+    
+#include "Equation_Solver.cpp"
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	cout << "Hello" << endl;
+	return 0;
+}
+
+
