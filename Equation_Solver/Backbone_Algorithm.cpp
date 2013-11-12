917968b93697ac0699efd9b263229330961e7a6f
John-Michael Reed <JohnReedLOL@ufl.edu>
2013-11-12 04:28:37 +0000

    Everything above "using namespace std" is modify-able
    Change the function and the min-x and max-t

--- Equation_Solver/Backbone_Algorithm.cpp
+++ Equation_Solver/Backbone_Algorithm.cpp
@@ -1,32 +1,31 @@
-//Compile with: g++ -std=c++11 -O3 -o Backbone_Algorithm ~/Backbone_Algorithm.cpp
+//Compile with: g++ -O3 -o Backbone_Algorithm ~/Backbone_Algorithm.cpp
 
-#include <cmath> 
-#include <float.h>  
+#include <cmath>
+#include <float.h>
 #include <iostream>
 #include <iomanip>
 
-using namespace std;
+//These constants determine the range for the x-values.
+const float x_begin = -32767.0f;
+const float x_end = 32767.0f;
 
 //Type the equation into this function.
 inline float equation(float x)
 {
-//For example, the equation pow(x,2)-4=0 has solutions +-2.
-return powf(x, 2.0f)-4.0f; 
+	return powf(x, 2.0f)-4.0f;
 }
 
+using namespace std;
+
 int main ()
-{	
-    std::cout.precision(30);
+{        
+    std::cout.precision(20);
     std::cout.setf( std::ios::fixed, std::ios::floatfield ); // floatfield set to fixed
-    
-    //const float c = 32767.0f;
     
     const float max = FLT_MAX;
     const float mm = 0.00024414062f;
 
-    //const float diff = 0.0f + 8.0f*nextafterf(0.0f,max);
-
-    float h= -32767.0f;
+    float h= x_begin;
     float j= powf (h, 2.0f)-4.0f;
 
     float k= abs(j);
@@ -35,51 +34,48 @@ int main ()
     float c;
     float l;
     bool hasSolution = false;
-	cout << '\n';
+    cout << '\n' << "Solutions: " << '\n';
     for(b = nextafterf(h,max);b < -0.0078125f; b = nextafterf(b,max) )
-    {
-	//c= powf(b, 2.0f)-4.0f; //The equation.	
-	c = equation(b);
-	l = abs(c);
-	if( k < mm )
-	{
-		if( (l>=k) && (hasSolution == false) )
-		{
-			std::cout << "+Solution: " << h << '\n' << "+Distance to x-axis: " << k << '\n' << '\n';
-			hasSolution = true;
-		}
-	}
-	else {hasSolution = false;}
-	k=l;
-	j=c;
-	h=b;
+    {     
+        c = equation(b);
+        l = abs(c);
+        if( k < mm )
+        {
+                if( (l>=k) && (hasSolution == false) )
+                {
+                        std::cout << h << '\n';
+                        hasSolution = true;
+                }
+        }
+        else {hasSolution = false;}
+        k=l;
+        j=c;
+        h=b;
     }
 
 hasSolution = false;
 h = 0.0f;
-//j = powf(h, 2.0f)-4.0f; //The equation.
 j = equation(h);
 k = abs(j);
 
-    for(b = 0.0078125f; b < 32767.0f; b = nextafterf(b,max) )
+    for(b = 0.0078125f; b < x_end; b = nextafterf(b,max) )
     {
-	//c= powf(b, 2.0f)-4.0f; //The equation.
-	c = equation(b);
-	l = abs(c);
+        c = equation(b);
+        l = abs(c);
 
-	if( k < mm )
-	{
-		if( (l>=k) && (hasSolution == false) )
-		{
-			std::cout << "-Solution: " << h << '\n' << "-Distance to x-axis: " << k << '\n' << '\n';
-			hasSolution = true;
-		}
-	}
-	else {hasSolution = false;}
-	k=l;
-	j=c;
-	h=b;
+        if( k < mm )
+        {
+                if( (l>=k) && (hasSolution == false) )
+                {
+                        std::cout << h << '\n';
+                        hasSolution = true;
+                }
+        }
+        else {hasSolution = false;}
+        k=l;
+        j=c;
+        h=b;
     }
-
-  return 0;
+cout << '\n';
+return 0;
 }
